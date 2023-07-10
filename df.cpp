cc08dcb66d55eaa07b221ac6a628f2255fe1ffda
sasipatson <139094335+sasipatson@users.noreply.github.com>
2023-07-10 10:42:13 +0700

    Add files via upload

---
+++ df.cpp
@@ -0,0 +1,13 @@
+#include <iostream> 
+using namespace std; 
+int main()
+	{ cout << "Student Code : 6606021620180" << endl;
+      cout << "Student Name : Sasipatson Saenaubon" << endl;
+	     cout << " Division : Information Technology" << endl;
+	         cout << " Year : 1" << endl;
+			 cout << " Room : RA" << endl;
+			  cout << " Sex : Female" << endl;
+			   cout << "Age : 18" << endl;
+			      system("pause"); 
+				  return(0); 
+}
